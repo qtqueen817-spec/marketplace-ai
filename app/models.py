a5686f7d8ebbfcf89dd19307c7fd417bdee4a12e
qtqueen817-spec <qtqueen817@gmail.com>
2026-03-11 05:18:42 +0000

    h

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from app.database import Base
+from app.database import Base # pyright: ignore[reportMissingImports]
 
 class User(Base):
     __tablename__ = "users"
