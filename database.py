d69fa2f3a0fdf73de4c0634b9a7368bf1c701965
qtqueen817-spec <qtqueen817@gmail.com>
2026-03-11 01:48:13 +0000

    j

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 import os
-from sqlalchemy import create_url
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# This URL matches the service name 'db' from your docker-compose.yaml
+# The 'db' hostname here must match the service name in docker-compose.yaml
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/marketplace")
 
 engine = create_engine(DATABASE_URL)
