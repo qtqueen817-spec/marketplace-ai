75891a747665b96fb3794e0d27ca41db90bd050e
qtqueen817-spec <qtqueen817@gmail.com>
2026-03-11 01:43:27 +0000

    h

--- database.py
+++ database.py
@@ -1,20 +1,11 @@
-from sqlalchemy import create_engine
+import os
+from sqlalchemy import create_url
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 
-# Replace with your actual database URL
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/dbname")
+# This URL matches the service name 'db' from your docker-compose.yaml
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@db:5432/marketplace")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
-# Dependency to get the DB session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
